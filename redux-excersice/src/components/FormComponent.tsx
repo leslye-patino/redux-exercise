d66c545831fb3da1e1e601fe504121624f101640
Leslye Patiño <leslye.patino@wizeline.com>
2023-02-17 16:59:05 -0600

    typescript and prettier

---
+++ redux-excersice/src/components/FormComponent.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function FormComponent() {
+    const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {}
+
+    return (
+        <div className="wrapper">
+            <form onSubmit={handleSubmit}>
+                <fieldset>
+                    <label>
+                        <p>Name</p>
+                        <input name="name" />
+                    </label>
+                </fieldset>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
--- redux-excersice/src/reducer.js
+++ redux-excersice/src/reducer.js
@@ -7,23 +7,22 @@
  * instead return a new state obj with updated values.
  */
 
- export default function reducer(state, action) {
-    const { type, payload } = action;
+export default function reducer(state, action) {
+    const { type, payload } = action
     switch (type) {
-      case "CHANGE_NAME": {
-        return {
-          ...state,
-          name: payload.name
-        };
-      }
-      case "RESET": {
-        return {
-          ...state,
-          name: ""
-        };
-      }
-      default:
-        return state;
+        case 'CHANGE_NAME': {
+            return {
+                ...state,
+                name: payload.name,
+            }
+        }
+        case 'RESET': {
+            return {
+                ...state,
+                name: '',
+            }
+        }
+        default:
+            return state
     }
-  }
-  
+}
