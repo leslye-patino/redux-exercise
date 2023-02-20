c0b28ccfee9dd0b25510769a30dc86aa152d635f
Martin Robledo <martin.robledo@wizeline.com>
2023-02-20 17:03:09 -0600

    feat: adding passwords fields and yup validation

--- redux-excersice/src/index.js
+++ redux-excersice/src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import ComponentA from "./components/ComponentA";
 import FormComponent from "./components/FormComponent";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <ComponentA />
     <FormComponent/>
   </React.StrictMode>,
   rootElement
